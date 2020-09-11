b1cde8b298414008a4fba63952770a00013c8e1a
mbelykh <mbelykh@phoenixit.ru>
2020-09-11 17:56:14 +0300

    Added logistic regression

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
-#%% Load dataset
+# %% Load dataset
 
-import matplotlib.pyplot as plt
 import pandas as pd
-import seaborn as sns
 
 CROSS_VALIDATION_ITER = 25
 
 data_file = "csv/breast_cancer/data.csv"
-dataset = pd.read_csv(data_file,index_col=0)
+dataset = pd.read_csv(data_file, index_col=0)
 dataset['diagnosis'] = pd.array(list(map(lambda x: 0.0 if x == 'M' else 1.0, dataset['diagnosis'])))
 
 del dataset["Unnamed: 32"]
 
 dataset.describe()
 
-ax = sns.PairGrid(dataset.iloc[:, 0:11], hue="diagnosis", palette="Set2")
-ax = ax.map_diag(plt.hist, edgecolor="w")
-ax = ax.map_offdiag(plt.scatter, edgecolor="w", s=40)
-plt.show()
+print(dataset.corr().abs())
+
+corr_matrix = dataset.corr().abs()
+do_not_drop = set()
+for row in corr_matrix.columns:
+    for column in corr_matrix.columns:
+        if column == row:
+            continue
+        if corr_matrix[row][column] > 0.85:
+            if column in dataset and column not in do_not_drop:
+                del dataset[column]
+                do_not_drop.add(row)
+
+print(dataset)
